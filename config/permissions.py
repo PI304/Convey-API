d7e10bf59d9ca74675c48d53691b9b945bb3fab1
linda2927 <linda2927@naver.com>
2023-02-27 14:10:15 +0900

    fix: separate permission for admin and app user for IsAdminOrReadOnly

--- config/permissions.py
+++ config/permissions.py
@@ -9,11 +9,11 @@ class IsAdminOrReadOnly(BasePermission):
     message = "Permission denied"
 
     def has_permission(self, request: Request, view: APIView) -> bool:
-        user = UserService.authenticate_by_token(request)
-        if (
-            request.method in SAFE_METHODS
-            or request.user
-            and request.user.is_authenticated
-        ):
-            return True
-        return False
+        if request.method in SAFE_METHODS:
+            if request.user:
+                return True
+        else:
+            if request.user.role == 1:
+                return True
+            else:
+                return False
