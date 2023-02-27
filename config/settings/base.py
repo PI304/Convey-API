8f1793663db18f3695a363e5cfb8ae45c1b3ce5a
linda2927 <linda2927@naver.com>
2023-02-27 22:59:01 +0900

    deploy: change ec2 ip to elastic ip

--- config/settings/base.py
+++ config/settings/base.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["13.124.108.220"]
+ALLOWED_HOSTS = ["3.34.67.68"]
 
 
 # Application definition
