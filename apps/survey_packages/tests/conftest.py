0ccdb93c065a4eaef05214bd3db916dd0d6f8f93
linda2927 <linda2927@naver.com>
2023-03-03 01:45:27 +0900

    test: add test codes for survey packages put method

--- apps/survey_packages/tests/conftest.py
+++ apps/survey_packages/tests/conftest.py
@@ -3,7 +3,6 @@ import pytest
 
 from apps.base_fixtures import *
 from apps.survey_packages.models import SurveyPackage, PackageContact
-from apps.surveys.models import Survey, SurveySector, SectorChoice, SectorQuestion
 
 
 @pytest.fixture(autouse=False, scope="function")
@@ -15,6 +14,20 @@ def sample_package_base_data():
             return data
     except FileNotFoundError:
         file_path = "tests/sample_data/base_package.json"
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            return data
+
+
+@pytest.fixture(autouse=False, scope="function")
+def sample_package_parts_data():
+    try:
+        file_path = "./apps/survey_packages/tests/sample_data/survey_parts.json"
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        file_path = "tests/sample_data/survey_parts.json"
         with open(file_path, "r") as file:
             data = json.load(file)
             return data
@@ -44,45 +57,4 @@ def create_empty_survey_packages(db):
     )
     PackageContact.objects.create(
         id=998, type="phone", content="01011112222", survey_package_id=999
-    )
-
-
-@pytest.fixture(autouse=False, scope="function")
-def create_sectors(db):
-    # Sector 1
-    SurveySector.objects.create(
-        id=999,
-        survey_id=999,
-        title="sector 1",
-        description="sector 1",
-        question_type="likert",
-    )
-
-    SectorChoice.objects.create(id=999, sector_id=999, key="1", value="매우 그렇다")
-    SectorChoice.objects.create(id=998, sector_id=999, key="2", value="그렇다")
-    SectorChoice.objects.create(id=997, sector_id=999, key="3", value="보통이다")
-    SectorChoice.objects.create(id=996, sector_id=999, key="4", value="그렇지 않다")
-    SectorChoice.objects.create(id=995, sector_id=999, key="5", value="매우 그렇지 않다")
-
-    SectorQuestion.objects.create(id=999, sector_id=999, content="질문1")
-    SectorQuestion.objects.create(id=998, sector_id=999, content="질문2")
-    SectorQuestion.objects.create(id=997, sector_id=999, content="질문3")
-
-    # Sector 2
-    SurveySector.objects.create(
-        id=998,
-        survey_id=999,
-        title="sector 2",
-        description="sector 2",
-        question_type="short_answer",
-    )
-
-    SectorChoice.objects.create(id=994, sector_id=998, key="일주일에", value="%d일")
-    SectorChoice.objects.create(id=993, sector_id=998, key="하루에", value="%d시간%d분")
-
-    SectorQuestion.objects.create(
-        id=996, sector_id=998, content="한번에 적어도 10분 이상 걸은 날은 며칠?"
-    )
-    SectorQuestion.objects.create(
-        id=995, sector_id=998, content="그런 날 중 하루에 걸으면서 보낸 시간?"
     )
--- apps/survey_packages/tests/test_survey_packages.py
+++ apps/survey_packages/tests/test_survey_packages.py
@@ -6,25 +6,40 @@ from rest_framework.test import APIRequestFactory, APIClient
 
 
 @pytest.mark.django_db
-def test_create_empty_survey_package(client_json_request, sample_package_base_data):
+def test_create_empty_survey_package(client_request, sample_package_base_data):
     url = "/api/survey-packages/"
     data = sample_package_base_data
-    res = client_json_request("post", url, data)
+    res = client_request("post", url, data)
 
     assert res.status_code == 201
     assert res.data["author"]["id"] == 999
 
 
 @pytest.mark.django_db
-def test_update_empty_package(client_json_request, create_empty_survey_packages):
+def test_update_empty_package(client_request, create_empty_survey_packages):
     url = "/api/survey-packages/999/"
     data = dict(
         description="updated description",
         contacts=[dict(type="email", content="updated@email.com")],
     )
 
-    res = client_json_request("patch", url, data)
+    res = client_request("patch", url, data)
 
     assert res.status_code == 200
     assert res.data["description"] == data["description"]
     assert len(res.data["contacts"]) == 1
+
+
+@pytest.mark.django_db
+def test_compose_survey_package(
+    client_request, create_empty_survey_packages, sample_package_base_data
+):
+    url = "/api/survey-packages/999/"
+    data = sample_package_base_data
+    res = client_request("put", url, data)
+
+    assert res.status_code == 200
+    assert res.data["id"] == 999
+    assert res.data["author"]["id"] == 999
+    assert len(res.data["contacts"]) == 2
+    assert len(res.data["parts"]) == 2
