d5f0254289e4de203a58fd04ceaeb68b30a8b61a
linda2927 <linda2927@naver.com>
2023-03-02 23:36:52 +0900

    refac: move global fixtures to a separate file

---
+++ apps/base_fixtures.py
@@ -0,0 +1,17 @@
+import pytest
+from rest_framework.test import APIClient
+
+from apps.users.models import User
+from config.client_request_for_test import ClientRequest
+
+
+@pytest.fixture(autouse=True, scope="session")
+@pytest.mark.django_db
+def client_request(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        user = User.objects.create(
+            id=999, email="email@test.com", name="test", password="12345678", role=0
+        )
+        client = APIClient()
+        client.force_authenticate(user)
+        return ClientRequest(client)
