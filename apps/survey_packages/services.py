f1b951fb56b0783faaf8b3954f76c6c7c9815c80
earthlyz9 <linda2927@naver.com>
2023-03-03 09:01:25 +0900

    Feat/survey packages put method (#18)
    * fix: add title field for subject-survey composition
    
    * feat: add services for adding survey package parts and subjects

--- apps/survey_packages/services.py
+++ apps/survey_packages/services.py
@@ -1,11 +1,15 @@
-import json
 from typing import Union, List
 
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import ValidationError
 
-from apps.survey_packages.models import SurveyPackage
-from apps.survey_packages.serializers import PackageContactSerializer
+from apps.survey_packages.models import SurveyPackage, PackagePart
+from apps.survey_packages.serializers import (
+    PackageContactSerializer,
+    PackagePartSerializer,
+    PackageSubjectSerializer,
+    PackageSubjectSurveySerializer,
+)
 
 
 class SurveyPackageService(object):
@@ -25,3 +29,53 @@ class SurveyPackageService(object):
                 serializer.save(survey_package_id=self.package.id)
 
         return self.package
+
+    def delete_related_components(self) -> None:
+        PackagePart.objects.filter(survey_package_id=self.package.id).delete()
+
+    def create_parts(self, data: list[dict]) -> list[PackagePart]:
+        created_parts = []
+        for d in data:
+            part = self._create_part(d)
+            created_parts.append(part)
+
+        return created_parts
+
+    def _create_part(self, part_data: dict, package_id: int) -> PackagePart:
+        # Create a part
+        if "title" not in part_data or part_data["title"] is None:
+            raise ValidationError("a part should have a title")
+
+        serializer = PackagePartSerializer(data=dict(title=part_data["title"]))
+        if serializer.is_valid(raise_exception=True):
+            part = serializer.save(survey_package_id=package_id)
+
+        # Create subjects
+        if "subjects" not in part_data or part_data["subjects"] is None:
+            raise ValidationError("a part should have a title")
+        self._create_subjects(part_data["subjects"], part.id)
+
+        return part
+
+    def _create_subjects(self, subjects_list: list[dict], part_id: int):
+        for subject in subjects_list:
+            self._create_subject(subject, part_id)
+
+    def _create_subject(self, subject_data: dict, part_id: int):
+        serializer = PackageSubjectSerializer(data=subject_data)
+        if serializer.is_valid(raise_exception=True):
+            subject = serializer.save(package_part_id=part_id)
+
+        self._associate_subject_with_surveys(
+            subject.id, subject_data.get("surveys", None)
+        )
+
+    def _associate_subject_with_surveys(
+        self, subject_id: int, data: list[dict]
+    ) -> None:
+        for ss in data:
+            serializer = PackageSubjectSurveySerializer(
+                data=dict(title=ss.get("title", None))
+            )
+            if serializer.is_valid(raise_exception=True):
+                serializer.save(subject_id=subject_id, survey_id=ss.get("survey", None))
