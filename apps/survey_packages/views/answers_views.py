d2dda59f27f22452aa8f2213d38aeaccd2906b89
earthlyz9 <linda2927@naver.com>
2023-03-09 16:40:05 +0900

    Feat/add details to swagger doc (#28)
    * feat: add camel case middleware for query strings
    
    * fix: delete unnecessary tables
    
    * feat: add openpyxl
    
    * feat: add respondent serializer
    
    * feat: record respondent data when response is submitted
    
    * feat: add service class for exporting data to excel
    
    * feat: add views and url for downloading response data
    
    * fix: check if survey package is added to workspace when allocating survey package to routine detail data
    
    * style: remove unnecessary import statements
    
    * fix: add routine info when submitting kickoff survey
    
    * style: remove unnecessary import statements
    
    * feat: remove refresh token from response schema

--- apps/survey_packages/views/answers_views.py
+++ apps/survey_packages/views/answers_views.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from django.db.models import QuerySet
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from drf_yasg import openapi
@@ -8,12 +9,23 @@ from rest_framework import generics, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apps.surveys.models import QuestionAnswer
 from apps.surveys.serializers import QuestionAnswerSerializer
 from apps.surveys.services import QuestionAnswerService
-from apps.workspaces.models import Workspace
+from apps.workspaces.models import (
+    Workspace,
+    WorkspaceComposition,
+    RoutineDetail,
+    Routine,
+)
+from apps.workspaces.serializers import (
+    WorkspaceCompositionSerializer,
+    RoutineSerializer,
+)
 from config.exceptions import InstanceNotFound
+from config.permissions import AdminOnly
 
 
 class SurveyPackageAnswerCreateView(generics.CreateAPIView):
@@ -24,6 +36,14 @@ class SurveyPackageAnswerCreateView(generics.CreateAPIView):
         tags=["survey-answer"],
         operation_summary="해당 survey package 에 대한 피험자의 응답을 생성합니다",
         operation_description="헤더의 Authorization 를 이용하여 앱 이용자임을 식별합니다",
+        manual_parameters=[
+            openapi.Parameter(
+                "routine",
+                openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="루틴 정보를 함께 받을 것인지 표기합니다. 루틴 정보를 함께 받으려면 ?routine=y 의 형태로 query string 을 포함시켜주세요. 받지 않으려면 query string 을 제외합니다",
+            )
+        ],
         request_body=openapi.Schema(
             type=openapi.TYPE_OBJECT,
             properties={
@@ -49,7 +69,20 @@ class SurveyPackageAnswerCreateView(generics.CreateAPIView):
             },
         ),
         responses={
-            201: openapi.Response("created", QuestionAnswerSerializer(many=True))
+            201: openapi.Schema(
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "answers": openapi.Schema(
+                        type=openapi.TYPE_ARRAY,
+                        description="생성한 응답들",
+                        items=openapi.Schema(type=openapi.TYPE_OBJECT),
+                    ),
+                    "routine": openapi.Schema(
+                        type=openapi.TYPE_OBJECT,
+                        description="/workspace/{id}/routines 와 동일",
+                    ),
+                },
+            )
         },
     )
     def post(self, request: Request, *args: Any, **kwargs: Any) -> Response:
@@ -70,7 +103,88 @@ class SurveyPackageAnswerCreateView(generics.CreateAPIView):
         )
 
         answers = service.create_answers(request.data)
+        service.record_respondent()
 
         serializer = self.get_serializer(answers, many=True)
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        routine_data = None
+        if request.GET.get("routine"):
+            try:
+                routine = get_object_or_404(Routine, workspace_id=workspace.id)
+                routine_data = RoutineSerializer(routine).data
+            except Http404:
+                pass
+
+        return Response(
+            {"answers": serializer.data, "routine": routine_data},
+            status=status.HTTP_201_CREATED,
+        )
+
+
+class SurveyPackageAnswerDownloadView(APIView):
+    permission_classes = [AdminOnly]
+    serializer_class = WorkspaceCompositionSerializer
+    queryset = WorkspaceComposition.objects.all()
+
+    def get_queryset(self) -> QuerySet:
+        survey_package_id = self.kwargs.get("pk")
+        workspace_query: str = self.request.GET.get("workspace")
+
+        if workspace_query is None:
+            raise ValidationError("workspace id not set in query string")
+
+        if not workspace_query.isnumeric():
+            raise ValidationError("workspace must be in number format")
+
+        workspace_id = int(workspace_query)
+
+        try:
+            workspace_composition = get_object_or_404(
+                WorkspaceComposition,
+                survey_package_id=survey_package_id,
+                workspace_id=workspace_id,
+            )
+        except Http404:
+            raise InstanceNotFound(
+                "survey package does not exist in the provided workspace"
+            )
+        return self.queryset.filter(
+            survey_package_id=survey_package_id, workspace_id=workspace_id
+        ).select_related("survey_package", "workspace")
+
+    def get_object(self):
+        return self.get_queryset().first()
+
+    @swagger_auto_schema(
+        operation_summary="survey package 의 응답을 엑셀 파일 형태로 다운로드 합니다",
+        tags=["survey-answer"],
+        manual_parameters=[
+            openapi.Parameter(
+                "id",
+                openapi.IN_PATH,
+                description="survey package id",
+                type=openapi.TYPE_INTEGER,
+                required=True,
+            ),
+            openapi.Parameter(
+                "workspace",
+                openapi.IN_QUERY,
+                description="다운로드하고자 하는 survey package 가 속해있는 workspace id",
+                type=openapi.TYPE_INTEGER,
+            ),
+        ],
+    )
+    def get(self, request, *args, **kwargs) -> Response:
+        instance: WorkspaceComposition = self.get_object()
+
+        routine_id = instance.workspace.routine.id
+        routine_detail: RoutineDetail = RoutineDetail.objects.filter(
+            routine_id=routine_id, survey_package_id=instance.id
+        ).first()
+
+        workspace_name: str = instance.workspace.name
+        package_name = instance.survey_package.title
+        nth_day = routine_detail.nth_day
+        time = routine_detail.time
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
