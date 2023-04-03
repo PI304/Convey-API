02e1c9f74be3b056801d93bbeec448747c48d260
linda2927 <linda2927@naver.com>
2023-04-04 08:18:52 +0900

    feat: add new domain to allowed hosts

--- config/settings/deploy.py
+++ config/settings/deploy.py
@@ -1,10 +1,6 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = [
-    "3.34.67.68",
-    "localhost",
-    "127.0.0.1",
-]  # 인스턴스 IPv4 기반 퍼블릭 DNS 주소 or 퍼블릭 IP
+ALLOWED_HOSTS = ["3.34.67.68", "api.convey.works", "convey.works"]
 
 WSGI_APPLICATION = "config.wsgi.deploy.application"
