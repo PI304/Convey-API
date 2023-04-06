dc9154f3e2a135fed94f92d984838fdea3c1c48a
linda2927 <linda2927@naver.com>
2023-04-07 08:59:53 +0900

    fix: typo in FE origin domain name

--- utils/body_encryption.py
+++ utils/body_encryption.py
@@ -9,22 +9,25 @@ load_dotenv()
 class AESCipher(object):
     def __init__(self):
         self.bs = AES.block_size
-        self.key = bytes(os.environ.get("AES256_KEY"), "utf-8")
-        self.iv = bytes(os.environ.get("AES256_IV"), "utf-8")
+        self.key = os.environ.get("AES256_KEY").encode()
+        self.iv = os.environ.get("AES256_IV").encode()
 
     def encrypt(self, raw):
         raw = self._pad(raw)
         cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
         return base64.b64encode(self.iv + cipher.encrypt(raw.encode()))
 
-    def decrypt(self, enc):
+    def decrypt(self, enc: bytes):
         enc = base64.b64decode(enc)
         cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        return self._unpad(cipher.decrypt(enc[AES.block_size :])).decode("utf-8")
+        return self._unpad(cipher.decrypt(enc[self.bs :])).decode("utf-8")
 
     def _pad(self, s):
         return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
 
     @staticmethod
-    def _unpad(s):
-        return s[: -ord(s[len(s) - 1 :])]
+    def _unpad(s: bytes):
+        padding_size = s[-1]
+        if padding_size > len(s):
+            raise ValueError("Invalid padding size.")
+        return s[:-padding_size]
