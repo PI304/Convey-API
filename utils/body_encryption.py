c688431e7500ecf58e2bb40729cd8e9f0f6ab402
Z9 <linda2927@naver.com>
2023-04-09 13:33:58 +0900

    Feat/aes implementation and cookie attributes (#45)
    * fix: make created_at and updated_at field nullable
    
    * feat: implement aes encryption via service layer

--- utils/body_encryption.py
+++ utils/body_encryption.py
@@ -12,10 +12,10 @@ class AESCipher(object):
         self.key = os.environ.get("AES256_KEY").encode()
         self.iv = os.environ.get("AES256_IV").encode()
 
-    def encrypt(self, raw):
-        raw = self._pad(raw)
+    def encrypt(self, raw: bytes):
+        padded = self._pad(raw)
         cipher = AES.new(self.key, AES.MODE_CBC, self.iv)
-        return base64.b64encode(self.iv + cipher.encrypt(raw.encode()))
+        return base64.b64encode(self.iv + cipher.encrypt(padded))
 
     def decrypt(self, enc: bytes):
         enc = base64.b64decode(enc)
@@ -23,11 +23,18 @@ class AESCipher(object):
         return self._unpad(cipher.decrypt(enc[self.bs :])).decode("utf-8")
 
     def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+        pad_size = self.bs - len(s) % self.bs
+        pad = bytes([pad_size] * pad_size)
+        return s + pad
 
-    @staticmethod
-    def _unpad(s: bytes):
+    def _unpad(self, s: bytes):
         padding_size = s[-1]
-        if padding_size > len(s):
+        if padding_size < 1 or padding_size > self.bs:
+            # 패딩 크기가 유효하지 않은 경우 에러를 발생시킵니다.
             raise ValueError("Invalid padding size.")
-        return s[:-padding_size]
+        padding_start = len(s) - padding_size
+        for i in range(padding_start, len(s)):
+            if s[i] != padding_size:
+                # 패딩 값이 유효하지 않은 경우 에러를 발생시킵니다.
+                raise ValueError("Invalid padding.")
+        return s[:padding_start]
