387e890d9528b30076b87e6718c86c29a96db832
linda2927 <linda2927@naver.com>
2023-04-09 19:33:37 +0900

    fix: add validation for social provider type

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -31,3 +31,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["password"] = make_password(validated_data.get("password"))
         return super(UserSerializer, self).create(validated_data)
+
+    def validate_social_provider(self, value):
+        if value and value not in User.SocialProviderType.labels:
+            raise ValidationError("social provider invalid")
+
+        return value
